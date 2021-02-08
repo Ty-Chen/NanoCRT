83a1fa67de6287876096357d6375af5643ed188d
TyChen <369494003@qq.com>
2021-02-08 06:05:57 -0800

    Update test source code

--- Test/test.c
+++ Test/test.c
@@ -2,22 +2,29 @@
 
 int main(long argc, char *argv[])
 {
-    long i;
-    FILE* fp;
-
+    FILE*  fp  = NULL;
+    char*  buf = "hello how are you \n";
+    char*  tmp = NULL;
+    long   len = strlen(buf);
+      
     fp = fopen("test.txt", "w");
 
-    //
-    {
-        char* buf = "hello how are you \n";
-        long len = strlen(buf);
-        fwrite(buf, 1, len, fp);
-        //fwrite(0, "c", 1);
-        printf("hello world\n");
-        printf("%d  %s\n", len, buf);
-    }
+    fwrite(buf, 1, len, fp);
+    printf("hello world\n");
+    printf("%d\n%s\n", len, buf);
 
     fclose(fp);
-
+    
+    tmp = (char*) malloc(strlen(buf));
+    if (tmp == NULL)
+    {
+    	return -1;
+    }
+    
+    tmp = strcpy(tmp, buf);  
+    printf("%s\n", tmp);
+    
+    free(tmp);
+    
     return 0;
 }
