53c66ac367d06767793e3598cfc7ad3856e36008
Ty-Chen <tianyuch@hotmail.com>
2021-02-23 00:14:49 +0800

    Update iostream.cpp

--- Src/iostream.cpp
+++ Src/iostream.cpp
@@ -7,5 +7,26 @@
 
 namespace std
 {
+    stdout_stream::stdout_stream() : ofstream()
+    {
+        fp = stdout;
+    }
+
+    stdout_stream cout;
+
+    ofstream::ofstream() : fp(0)
+    {
+    }
+
+    ofstream::ofstream(const char* fileName, ofstream::openmode md) : fp(0)
+    {
+        open(fileName, md);
+    }
+
+    ofstream::~ofstream()
+    {
+        close();
+    }
+
 
 }
